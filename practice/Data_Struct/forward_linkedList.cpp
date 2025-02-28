e8d4c6f9f8143099f66671759edae2dd0b9c3e96
arinavanesian <arinavanoosyan@gmail.com>
2025-03-01 00:07:20 +0400

    forward_linkedList checked

--- practice/Data_Struct/forward_linkedList.cpp
+++ practice/Data_Struct/forward_linkedList.cpp
@@ -24,6 +24,9 @@ int main() {
 	trooper3.prefix[0] = 'L'; 
 	trooper3.prefix[1] = 'S'; 
 	trooper3.operating_number = 005;
+	trooper1.prefix[0] = 'T'; 
+	trooper1.prefix[1] = 'K'; 
+	trooper1.operating_number = 232; 
 	for (Element *cursor = &trooper1; cursor; cursor = cursor->next) { 
 		printf("stormtrooper %c%c-%d\n", cursor->prefix[0], cursor->prefix[1], cursor->operating_number); }
 		 }
